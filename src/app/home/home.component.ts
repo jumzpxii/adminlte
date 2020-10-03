2fbaafa1b90da6214a8f26012880d1de33fc2228
jumzpxii <porngsiri.jumjy@gmail.com>
2020-10-03 18:58:46 +0700

    jquery

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title = 'app works!';
 
   constructor() { }
+
+  ngAfterViewInit(){
+        $(document).ready(function(){
+          $("p").click(function(){
+          $(this).hide();
+          });
+        });
+  }
 
   ngOnInit(): void {
   }
 
+  selectDate(){
+    $('#reservationtime').daterangepicker({
+      timePicker: true,
+      timePickerIncrement: 30,
+      locale: {
+        format: 'MM/DD/YYYY hh:mm A'
+      }
+    })
+  }
 }
